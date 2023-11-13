045e9a153a685b72b8d41bbbf0f827f48c226a2f
Caroline Sampaio <caroline_08022@hotmail.com>
2023-11-13 17:18:44 -0300

    feat: adiciona ex 3 - rota post - cadastro pessoa

--- atividades-semana-6/app/Http/Controllers/PessoasController.php
+++ atividades-semana-6/app/Http/Controllers/PessoasController.php
@@ -15,4 +15,21 @@ class PessoasController extends Controller {
             return $this->response($exception->getMessage(), null, false, 500);
         }
     }
+
+
+    public function store(Request $request) {
+        try {
+            $request->validate([
+                'name' => 'required | min: 3 | max: 150',
+                'cpf' => 'min: 11 | max: 20',
+                'contact' => 'max: 20',
+            ]);
+
+            $pessoa = Pessoa::create($request->all());
+            $message = $pessoa->name . " cadastrada com sucesso";
+            return $this->response($message, $pessoa);
+        } catch (\Exception $execption) {
+            return $this->response($execption->getMessage(), null, false, 500);
+        }
+    }
 }
