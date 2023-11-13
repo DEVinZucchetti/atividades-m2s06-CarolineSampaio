df3b50aa705215961eee3d5e90563387066c8f49
Caroline Sampaio <caroline_08022@hotmail.com>
2023-11-13 17:44:39 -0300

    feat: adiciona ex 5 - rota DELETE - remove pessoa

--- atividades-semana-6/routes/api.php
+++ atividades-semana-6/routes/api.php
@@ -16,4 +16,4 @@ use App\Http\Controllers\PessoasController;
 */
 
 Route::resource('pessoas', PessoasController::class)
-    ->only(['index', 'store', 'update']);
+    ->only(['index', 'store', 'update', 'destroy']);
